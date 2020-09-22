08fe550440d44bd68556fe78558af6699b58734b
jessica helem <jessicahelem.s@hotmail.com>
2020-09-22 02:39:38 -0300

    inserindo_cenas e script

---
+++ bib_quiz/Assets/scripts/comandosBasicos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class comandosBasicos : MonoBehaviour
+{
+
+
+    public void carregaCena(string nomeCena)
+    {
+        SceneManager.LoadScene(nomeCena);
+
+    }
+    public void resetarPontuacao()
+    {
+
+        PlayerPrefs.DeleteAll();
+    }
+
+}
